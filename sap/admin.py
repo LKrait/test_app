dac118f2d250e4dcc2b3e302dc533a7199075d80
Kevin Afio <admin@example.com>
2023-01-20 15:28:37 +1000

    Chenged view page.

--- sap/admin.py
+++ sap/admin.py
@@ -8,3 +8,9 @@ admin.site.register(models.AcademicYear, AcademicYearAdmin)
 admin.site.register(models.Term)
 admin.site.register(models.Week)
 admin.site.register(models.Subject)
+
+
+#Configuring admin title
+admin.site.site_header = "SAP"
+admin.site.site_title = "SAP"
+admin.site.index_title = "Welcome to SAP"
--- sap/views.py
+++ sap/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+frpm django.contrib.auth import login, logout
 
 
 
